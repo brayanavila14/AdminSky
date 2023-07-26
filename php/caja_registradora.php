df52a1cd1092f182c0538f4e571bdf802db865f2
brayanavila14 <brayandavidavilaorozco@gmail.com>
2023-07-25 20:57:05 -0500

    upload v1.0.0

--- php/caja_registradora.php
+++ php/caja_registradora.php
@@ -8,26 +8,41 @@
     <title>Caja registradora - AdminSky</title>
 </head>
 <body>
-    <div class="encabezado">
-        <h2>Caja registradora</h2>
-    </div>
-    <form  method="post" class="inputs">
-        <input id="codigo_input" type="text" placeholder="Código" maxlength="5" autocomplete="off">
-        <div id="resultados_busqueda"></div>
-        <input id="producto_input" type="text" placeholder="Producto" disabled>
-        <input id="precio_input" type="number" placeholder="Precio" disabled>
-        <input id="cantidad_input" type="number" placeholder="Cantidad" autocomplete="off">
-        <input id="ingresar_producto" type="submit" value="Ingresar">
-    </form>
-    <button id="btnEliminar" type="button">Eliminar tabla</button>
-    <div class="campo1">
-    </div>
-    <div class="footer">
-        <input id="total_input" type="number" disabled>
+    <div class="container">
+        <div class="header">
+            <h2>Caja registradora</h2>
+        </div>
+        <div class="calculator">
+            <form method="post" class="inputs">
+                <input id="codigo_input" type="text" placeholder="Código" maxlength="5" autocomplete="off">
+                <div id="resultados_busqueda"></div>
+                <input id="producto_input" type="text" placeholder="Producto" disabled>
+                <input id="precio_input" type="number" placeholder="Precio" disabled>
+                <input id="cantidad_input" type="number" placeholder="Cantidad" autocomplete="off">
+                <button id="ingresar_producto" type="submit">Ingresar</button>
+            </form>
+            <div class="table-container">
+                <table class="tabla-datos">
+                    <tr class="encabezado">
+                        <th>Código</th>
+                        <th>Producto</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                    </tr>
+                </table>
+            </div>
+            <div class="total">
+                <input id="total_input" type="number" disabled>
+            </div>
+        </div>
+        <button id="btnEliminar" type="button">Eliminar tabla</button>
     </div>
     <script src="../library/jquery-3.7.0.js"></script>
     <script src="../js/caja_registradora.js"></script>
 </body>
 </html>
+
+
 
 
