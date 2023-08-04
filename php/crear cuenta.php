3cd97be644db29a13a9228faa0e600c2fb95690a
brayanavila14 <brayandavidavilaorozco@gmail.com>
2023-08-04 16:47:52 -0500

    upload v1.0.0

--- php/crear cuenta.php
+++ php/crear cuenta.php
@@ -1,20 +1,18 @@
 <?php
         include('base-de-datos.php');
-
     // Obtener datos del formulario
     $numeroId = trim($_POST['numberId']);
     $nombre = trim($_POST['name']);
     $email = trim($_POST['email']);
     $direccion = trim($_POST['direccion']);
-
 if (isset($_POST['crearCuenta'])) {
     // Consulta SQL para insertar los datos en la tabla de cuentas
-$insertar_cuenta = "INSERT INTO creditos (Id, nombre, email, direccion) VALUES ('$numeroId', '$nombre', '$email', '$direccion')";
-$resultado_cuenta = mysqli_query($conexion, $insertar_cuenta);
+    $insertar_cuenta = "INSERT INTO creditos (Id, nombre, email, direccion) VALUES ('$numeroId', '$nombre', '$email', '$direccion')";
+    $resultado_cuenta = mysqli_query($conexion, $insertar_cuenta);
     if ($resultado_cuenta) {
         echo "Cuenta creada con éxito.";
     } else {
-        echo "Error al crear la cuenta: " . $conn->error;
+        echo "Error al crear la cuenta: ";
     }
 }
 mysqli_close($conexion);
